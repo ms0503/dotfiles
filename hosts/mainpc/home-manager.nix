8be6829fb9d91e55e268c42668565de460c83d85
Sora Tonami <ms0503@outlook.com>
2024-05-05 19:43:53 +0900

    add .NET 8

--- hosts/mainpc/home-manager.nix
+++ hosts/mainpc/home-manager.nix
@@ -1,7 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  dotnet = pkgs.dotnet-sdk_8;
+in {
+  home.packages = [
+    dotnet
+  ];
   imports = [
     ../../home-manager/cli
     ../../home-manager/gui
     ../../home-manager/desktop/gnome
   ];
+  programs.bash.profileExtra = (builtins.readFile ../../home-manager/cli/bash/.profile) + ''
+    export DOTNET_ROOT=${dotnet}
+  '';
 }
