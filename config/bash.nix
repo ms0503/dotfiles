d4b187c061bd19608af02124561e9cf409348187
Sora Tonami <ms0503@outlook.com>
2024-09-11 13:46:34 +0900

    add .bash_logout

--- config/bash.nix
+++ config/bash.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   home.file = {
     "${config.home.homeDirectory}/.bash_aliases".source = ./bash/.bash_aliases;
+    "${config.home.homeDirectory}/.bash_logout".source = ./bash/.bash_logout;
     "${config.home.homeDirectory}/.bash_profile".source = ./bash/.bash_profile;
     "${config.home.homeDirectory}/.bashrc".source = ./bash/.bashrc;
     "${config.home.homeDirectory}/.profile".source = ./bash/.profile;
