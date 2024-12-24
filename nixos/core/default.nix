5bdae0d8d89517f5d3abd7eb368f496cd2f5cae8
ms0503 <ms0503@outlook.com>
2024-12-24 23:07:19 +0900

    add udisks2

--- nixos/core/default.nix
+++ nixos/core/default.nix
@@ -5,6 +5,7 @@
     ./network.nix
     ./nix.nix
     ./security.nix
+    ./udisks2.nix
     ./virt.nix
   ];
 }
---
+++ nixos/core/udisks2.nix
@@ -0,0 +1,7 @@
+{
+  services.udisks2 = {
+    enable = true;
+    mountOnMedia = true;
+  };
+}
+# vim: et sts=2 sw=2 ts=2
