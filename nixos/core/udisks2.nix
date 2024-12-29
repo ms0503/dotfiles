352e58b648753d3fbc59f1c9601cd93570518b0e
ms0503 <ms0503@outlook.com>
2024-12-29 22:39:07 +0900

    enable auto mount

--- nixos/core/udisks2.nix
+++ nixos/core/udisks2.nix
@@ -1,7 +1,12 @@
 {
-  services.udisks2 = {
-    enable = true;
-    mountOnMedia = true;
+  services = {
+    udev.extraRules = ''
+      ENV{UDISKS_AUTO}="1"
+    '';
+    udisks2 = {
+      enable = true;
+      mountOnMedia = true;
+    };
   };
 }
 # vim: et sts=2 sw=2 ts=2
