a7ef4b513bd45acd7f76ab4b8fe69e73dd91fe60
ms0503 <ms0503@outlook.com>
2024-12-29 18:04:18 +0900

    fix swipe gestures' direction

--- home-manager/desktop/hyprland/gestures.nix
+++ home-manager/desktop/hyprland/gestures.nix
@@ -1,8 +1,8 @@
 { config, ... }:
 {
   home.file."${config.xdg.configHome}/libinput-gestures.conf".text = ''
-    gesture swipe left 3 hyprctl dispatch workspace m-1
-    gesture swipe right 3 hyprctl dispatch workspace m+1
+    gesture swipe left 3 hyprctl dispatch workspace m+1
+    gesture swipe right 3 hyprctl dispatch workspace m-1
     gesture swipe up 3 wofi --show drun --width 512px
   '';
 }
