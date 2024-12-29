b32d9dca53ea8f0c4792a5cfd57fb8bc2634ffcc
ms0503 <ms0503@outlook.com>
2024-12-29 17:06:41 +0900

    add Proton GE Custom (original and RTSP patched version)

--- nixos/gui/steam.nix
+++ nixos/gui/steam.nix
@@ -1,8 +1,16 @@
-{ config, lib, ... }:
+{ config, inputs, lib, pkgs, ... }:
+let
+  myPkgs = inputs.self.outputs.packages.${pkgs.system};
+in
 lib.mkIf config.ms0503.steam.enable {
   programs.steam = {
     dedicatedServer.openFirewall = true;
     enable = true;
+    extraCompatPackages = (with pkgs; [
+      proton-ge-bin
+    ]) ++ (with myPkgs; [
+      proton-ge-rtsp-bin
+    ]);
     remotePlay.openFirewall = true;
   };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
 {
   microsoft-edge-dev = callPackage ./microsoft-edge-dev { };
   milk-cursor-themes = callPackage ./milk-cursor-themes { };
+  proton-ge-rtsp-bin = callPackage ./proton-ge-rtsp-bin { };
   unzip-unicode = callPackage ./unzip-unicode { };
   urxvt-wrapper = callPackage ./urxvt-wrapper { };
   zifu = callPackage ./zifu { };
