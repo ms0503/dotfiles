3dd9c7fb1d0e03e77dc06d470f8a7d549e5d8058
Sora Tonami <ms0503@outlook.com>
2025-02-10 19:03:29 +0900

    fix Spotify flickering

--- home-manager/gui/media.nix
+++ home-manager/gui/media.nix
@@ -1,13 +1,37 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  spotify-wayland = pkgs.spotify.overrideAttrs (
+    _: _: {
+      preFixup = ''
+        gappsWrapperArgs+=(
+          --add-flags "--enable-wayland-ime"
+          --add-flags "--disable-gpu-compositing"
+        )
+      '';
+    }
+  );
+in
 {
-  home.packages = with pkgs; [
-    evince
-    rosegarden
-    spotify
-    totem
-    vlc
-    vmpk
-  ];
+  home.packages =
+    with pkgs;
+    [
+      evince
+      rosegarden
+      totem
+      vlc
+      vmpk
+    ]
+    ++ (
+      if config.ms0503.wayland.enable then
+        [
+          spotify-wayland
+        ]
+      else
+        with pkgs;
+        [
+          spotify
+        ]
+    );
   programs = {
     ncspot.enable = true;
     obs-studio.enable = true;
