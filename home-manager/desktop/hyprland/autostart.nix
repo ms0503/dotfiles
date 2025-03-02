e6d0cb819bcc966455fca1c90744de669ff3eb9c
Sora Tonami <ms0503@outlook.com>
2025-03-02 18:27:30 +0900

    feature: hm: DE/Hyprland: autostart: add auto-restarting waybar

--- home-manager/desktop/hyprland/autostart.nix
+++ home-manager/desktop/hyprland/autostart.nix
@@ -8,22 +8,28 @@ let
   inherit (config.ms0503) terminal;
   inherit (pkgs) writeScriptBin;
   inherit (theme) colors;
-  sleep = writeScriptBin "sleep.sh" ''
+  sleep = writeScriptBin "sleep" ''
     swayidle -w \
       after-resume 'systemctl --user start libinput-gestures' \
       before-sleep 'systemctl --user stop libinput-gestures' \
       before-sleep 'swaylock -f -c ${colors.bg}' \
       &
   '';
+  waybar-wrapper = writeScriptBin "waybar-wrapper" ''
+    while :; do
+      waybar
+      sleep 5
+    done
+  '';
 in
 {
   wayland.windowManager.hyprland.settings.exec-once = [
     "dbus-update-activation-environment --systemd --all"
     "systemctl --user start hyprland-session.target"
-    "${sleep}/bin/sleep.sh"
+    "${sleep}/bin/sleep"
     "blueman-applet"
     "nm-applet"
-    "waybar"
+    "${waybar-wrapper}/bin/waybar-wrapper"
     "swww init && swww img ~/.config/hypr/wallpaper/bg.jpg"
     "fcitx5 -D"
     "steam -silent"
