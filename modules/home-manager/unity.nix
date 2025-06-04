1fd91e4d3b59200a7e9efa8aa0e9e06fa04fbe62
Sora Tonami <ms0503@outlook.com>
2025-06-05 02:48:09 +0900

    fix: hm-mod/unity: can't run alcom on wl

--- modules/home-manager/unity.nix
+++ modules/home-manager/unity.nix
@@ -11,13 +11,35 @@ in
 {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
+      (alcom.overrideAttrs (
+        _:
+        {
+          nativeBuildInputs ? [ ],
+          postInstall ? "",
+          ...
+        }:
+        {
+          nativeBuildInputs =
+            nativeBuildInputs
+            ++ (with pkgs; [
+              makeBinaryWrapper
+            ]);
+          # See https://github.com/tauri-apps/tauri/issues/10702
+          postInstall =
+            postInstall
+            + lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+              wrapProgram "$out/bin/ALCOM" \
+                --set __GL_THREADED_OPTIMIZATIONS 0 \
+                --set __NV_DISABLE_EXPLICIT_SYNC 1
+            '';
+        }
+      ))
       (unityhub.override {
         extraLibs =
           pkgs: with pkgs; [
             openssl_1_1
           ];
       })
-      alcom
     ];
   };
   options.ms0503.unity.enable = mkEnableOption "Unity and some applications";
