b51b750e038b9661a68e8aee9d41c112fe68d47a
Sora Tonami <ms0503@outlook.com>
2025-10-30 08:38:42 +0900

    fix: nyx-win: reference non-existent files

--- machines/nyx-win/nixos.nix
+++ machines/nyx-win/nixos.nix
@@ -6,27 +6,23 @@
 }:
 {
   environment.systemPackages = [ ];
-  imports = [
-    ../../nixos/core
-    ../../nixos/gui
-  ]
-  ++ (with inputs.nixos-hardware.nixosModules; [
-    common-cpu-amd
-    common-cpu-amd-pstate
-    common-cpu-amd-zenpower
-    common-gpu-nvidia-nonprime
-    common-hidpi
-    common-pc
-    common-pc-ssd
-  ]);
+  imports =
+    [ ]
+    ++ (with inputs.nixos-hardware.nixosModules; [
+      common-cpu-amd
+      common-cpu-amd-pstate
+      common-cpu-amd-zenpower
+      common-gpu-nvidia-nonprime
+      common-hidpi
+      common-pc
+      common-pc-ssd
+    ]);
   security.pam.services.greetd.enableGnomeKeyring = true;
-  services = {
-    xserver.xkb = {
-      layout = "jp";
-      model = "pc105";
-      options = "caps:none,compose:paus";
-      variant = "OADG109A";
-    };
+  services.xserver.xkb = {
+    layout = "jp";
+    model = "pc105";
+    options = "caps:none,compose:paus";
+    variant = "OADG109A";
   };
   system.stateVersion = "25.05";
   users.users."${username}" = {
