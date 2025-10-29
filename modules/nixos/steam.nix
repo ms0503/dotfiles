c2d34ed9e3d3cc62fb6a4f263f07b3cf9a5d1872
Sora Tonami <ms0503@outlook.com>
2025-10-29 17:26:34 +0900

    feature: remove monado

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -11,16 +11,6 @@ let
 in
 {
   config = mkIf cfg.enable {
-    boot.kernelPatches = [
-      {
-        name = "amdgpu-ignore-ctx-privileges";
-        patch = pkgs.fetchpatch {
-          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-          name = "cap_sys_nice_begone.patch";
-          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        };
-      }
-    ];
     environment.systemPackages = with pkgs; [
       wlx-overlay-s
     ];
@@ -50,17 +40,6 @@ in
         ];
         remotePlay.openFirewall = true;
       };
-    };
-    services.monado = {
-      defaultRuntime = true;
-      enable = true;
-      highPriority = true;
-    };
-    systemd.user.services.monado.environment = {
-      U_PACING_COMP_MIN_TIME_MS = "5";
-      # XRT_COMPOSITOR_COMPUTE = "1";
-      XRT_COMPOSITOR_FORCE_NVIDIA_DISPLAY = "Sony SIE  VRH";
-      XRT_COMPOSITOR_FORCE_WAYLAND = "1";
     };
   };
 }
