58446bbd5eca3f655d842cd3258ccdf0fc96fe86
Sora Tonami <ms0503@outlook.com>
2025-11-03 23:50:02 +0900

    feature: nixos/gui/utils: add vial and udev rules

--- modules/nixos/gui/utils.nix
+++ modules/nixos/gui/utils.nix
@@ -1,10 +1,27 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
   cfg = config.ms0503.gui;
 in
 {
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      vial
+    ];
     programs.dconf.enable = true;
+    services.udev.packages = [
+      (pkgs.writeTextFile {
+        destination = "/etc/udev/rules.d/59-vial.rules";
+        name = "vial-udev-rules";
+        text = ''
+          KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0666", TAG+="uaccess", TAG+="udev-acl"
+        '';
+      })
+    ];
   };
 }
