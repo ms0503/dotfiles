80fdda0fc066f8568e7332da1f3f1dced7460ecf
Sora Tonami <ms0503@outlook.com>
2026-04-21 13:52:04 +0900

    chore: hm/gui/gtk: set gtk4 theme explicitly (upstream changes)

--- modules/home-manager/gui/gtk.nix
+++ modules/home-manager/gui/gtk.nix
@@ -23,7 +23,10 @@ in
         gtk-application-prefer-dark-theme=true
       '';
       gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-      gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
+      gtk4 = {
+        inherit (config.gtk) theme;
+        extraConfig.gtk-application-prefer-dark-theme = true;
+      };
       iconTheme = {
         name = "Yaru-dark";
         package = pkgs.yaru-theme;
