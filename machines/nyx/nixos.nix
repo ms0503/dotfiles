2ecac214eab18b75a4349b2fc81ebe879243cf54
Sora Tonami <ms0503@outlook.com>
2026-05-29 09:21:10 +0900

    chore: nixos on erebos and nyx: remember last logged in session

--- machines/nyx/nixos.nix
+++ machines/nyx/nixos.nix
@@ -38,7 +38,7 @@
       enable = true;
       settings.default_session = {
         command = ''
-          ${pkgs.tuigreet}/bin/tuigreet --time
+          ${pkgs.tuigreet}/bin/tuigreet --remember --remember-session --time
         '';
         user = username;
       };
