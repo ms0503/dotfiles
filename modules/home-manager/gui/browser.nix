677a1283f38d9f2f78400feb94abcfe309cfade7
Sora Tonami <ms0503@outlook.com>
2026-07-20 16:53:30 +0900

    chore: hm/gui/browser: always use microsoft-edge-dev even when in wayland

--- modules/home-manager/gui/browser.nix
+++ modules/home-manager/gui/browser.nix
@@ -12,7 +12,7 @@ in
 {
   config = mkIf cfg.enable {
     home.packages = with myPkgs; [
-      (if config.ms0503.wayland.enable then microsoft-edge-dev-wayland else microsoft-edge-dev)
+      microsoft-edge-dev
     ];
     programs.firefox = {
       configPath = "${config.xdg.configHome}/mozilla/firefox";
