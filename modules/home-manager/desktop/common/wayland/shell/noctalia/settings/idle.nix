02ce086a915ad30352e3223dc80ba99e5e73649f
Sora Tonami <ms0503@outlook.com>
2026-08-03 04:23:34 +0900

    feature: hm/de/common/wl/shell/noctalia: remove auto suspend

--- modules/home-manager/desktop/common/wayland/shell/noctalia/settings/idle.nix
+++ modules/home-manager/desktop/common/wayland/shell/noctalia/settings/idle.nix
@@ -10,15 +10,9 @@ _: {
       enabled = true;
       timeout = 660;
     };
-    suspend = {
-      action = "lock_and_suspend";
-      enabled = true;
-      timeout = 900;
-    };
   };
   behavior_order = [
     "lock"
-    "suspend"
     "screen-off"
   ];
   pre_action_fade_seconds = 2.0;
